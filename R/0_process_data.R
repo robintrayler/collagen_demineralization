7612827b9ff45248b1fb63a1f293fbbc34e5b345
Robin Trayler <robintrayler@gmail.com>
2022-05-18 14:09:41 -0700

    incorporating Soras comments

--- R/0_process_data.R
+++ R/0_process_data.R
@@ -11,9 +11,9 @@ pb <- progress_bar$new(
   total = length(list.files(path = './data/goat_ATR/')))
 
 goat_data <- list.files(path = './data/goat_ATR/', 
-                        full.names = TRUE) |> 
-  map(process_spectra) |>  
-  reduce(rbind) |> 
+                        full.names = TRUE) %>% 
+  map(process_spectra)  %>%  
+  reduce(rbind)  %>% 
   write_csv(file = './data/processed_spectra/goat_data.csv')
 
 # deer data
@@ -21,9 +21,9 @@ pb <- progress_bar$new(
   total = length(list.files(path = './data/deer_ATR/')))
 
 deer_data <- list.files(path = './data/deer_ATR/', 
-                        full.names = TRUE) |> 
-  map(process_spectra) |>  
-  reduce(rbind) |> 
+                        full.names = TRUE)  %>% 
+  map(process_spectra)  %>%  
+  reduce(rbind)  %>% 
   write_csv(file = './data/processed_spectra/deer_data.csv')
 
 # shark data
@@ -31,8 +31,8 @@ pb <- progress_bar$new(
   total = length(list.files(path = './data/shark_ATR/')))
 
 shark_data <- list.files(path = './data/shark_ATR/', 
-                         full.names = TRUE) |>
-  map(process_spectra) |>
-  reduce(rbind) |>
+                    	 full.names = TRUE)  %>%
+  map(process_spectra)  %>%
+  reduce(rbind)  %>%
   write_csv(file = './data/processed_spectra/shark_data.csv')
 
